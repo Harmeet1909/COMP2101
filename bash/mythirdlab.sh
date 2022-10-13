e81eec889d8195da7f510932a75f0ee4cc8bd553
Harmeet1909 <harmeet.kaur3@mygeorgian.ca>
2022-10-13 01:13:11 -0400

    This is my third challenge script

--- bash/mythirdlab.sh
+++ bash/mythirdlab.sh
@@ -2,7 +2,7 @@
 
 #This is my third challenge script for this class.
 
-#Checking to see if I have lxd already installed
+###Checking to see if I have lxd already installed
 #Using which command to see if I have it already
 which lxd >/dev/null
 if [ $? -ne 0 ]; then
@@ -14,7 +14,7 @@ else
 fi
 
 
-#Testing to see if I have lxdbr0 interface
+###Testing to see if I have lxdbr0 interface
 #Searching route table to see if I have lxdbr0 interface already
 #Need to run lxd init --auto if no lxdbr0 is found
 ip r | grep lxdbr0 > /dev/null
@@ -27,7 +27,7 @@ else
 fi
 
 
-#Launching a container running Ubuntu 20.04 server named COMP2101-S22
+###Launching a container running Ubuntu 20.04 server named COMP2101-S22
 #Using which command to see if I have lxc existing
 which lxc >/dev/null
 if [ $? = 0 ]; then
@@ -39,4 +39,42 @@ else
 fi
 
 
+###Adding the container's current IP address in /etc/hosts
+#Setting variable for the required IP address
+ip=$(lxc list | grep -v IPV4 | awk '{print$6}')
+
+
+###Checking to see if I have apache2 already installed
+#Using which command to see if I have it already
+which apache2 >/dev/null
+if [ $? -ne 0 ]; then
+#I don't have apache2 already installed, so I need to install it now
+               sudo apt install apache2
+else
+#I already have apache2
+               echo "apache2 is already installed"
+fi
+
+
+###Retrieving the default web page from container's web service
+#Running which command to see if I have curl already installed
+which curl >/dev/null
+if [ $? -ne 0 ]; then
+#I don't have curl already installed, so I need to install it now
+                sudo apt install curl
+else
+#I already have curl 
+#Running curl http://COMP2101-S22 command to retrieve the web page
+                curl http://COMP2101-S22
+                if [ $? -ne 0 ];then
+                                echo 'Failed to retrieve the default web page'
+                else
+                                echo 'Succeeded in retrieving the default web page'
+                fi
+fi
+ 
+
+ 
+                
+ 
               
