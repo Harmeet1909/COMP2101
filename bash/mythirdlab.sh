d593bb95f76ec4c5c564f513a0e4b57fbb5b73b5
Harmeet1909 <harmeet.kaur3@mygeorgian.ca>
2022-10-20 12:40:24 -0400

    Finished completing my third challenge script

--- bash/mythirdlab.sh
+++ bash/mythirdlab.sh
@@ -2,7 +2,7 @@
 
 #This is my third challenge script for this class.
 
-###Checking to see if I have lxd already installed
+#####Checking to see if I have lxd already installed
 #Using which command to see if I have it already
 which lxd >/dev/null
 if [ $? -ne 0 ]; then
@@ -14,7 +14,9 @@ else
 fi
 
 
-###Testing to see if I have lxdbr0 interface
+
+
+#####Testing to see if I have lxdbr0 interface
 #Searching route table to see if I have lxdbr0 interface already
 #Need to run lxd init --auto if no lxdbr0 is found
 ip r | grep lxdbr0 > /dev/null
@@ -27,26 +29,55 @@ else
 fi
 
 
-###Launching a container running Ubuntu 20.04 server named COMP2101-S22
-#Using which command to see if I have lxc existing
-which lxc >/dev/null
-if [ $? = 0 ]; then
-#I already have lxc 
-              lxc launch ubuntu:20.04 COMP2101-S22
+
+
+#####Launching a container running Ubuntu 20.04 server named COMP2101-S22
+#Using lxc list to verify whether if I have the container or not
+sudo lxc list COMP2101
+if [ $? -eq 0 ]; then
+#I have the container already,so exit with an echo message
+               echo "You already have COMP2101-S22 container."
 else
-#I did not find lxc yet, so exit with an echo message
-              echo "Oops! You don't even have lxc yet."
+#Need to launch the container
+                lxc launch ubuntu:20.04 COMP2101-S22
+                if [ $? -eq 0 ];then
+                #Successfully launched
+                              echo "The required container has been successfully launched."
+                else
+                #Failed to launch, notify user and exit
+                              echo "COMP2101-S22 could not be made and thus failed to launch." >&2
+                              exit 1
+                fi
 fi
 
 
-###Adding the container's current IP address in /etc/hosts
-#Setting variable for the required IP address
-ip=$(lxc list | grep -v IPV4 | awk '{print$6}')
 
 
-###Checking to see if I have apache2 already installed
+#####Adding the container's current IP address in /etc/hosts
+#Checking to see if COMP2101-S22 is already there using grep command
+grep COMP2101-S22 /etc/hosts >/dev/null
+if [ $? -eq 0 ];then
+#Current IP address of COMP2101-S22 is already there in /etc/hosts
+              echo "You already have the current ip address of COMP2101-S22 in /etc/hosts."
+else
+#Need to add IP in /etc/hosts
+              lxc list -f compact -c 4n COMP2101-S22 | grep -v IPV4 | sudo tee -a /etc/hosts
+              if [ $? -eq 0 ];then
+              #Successfully added
+                            echo "The current IP address of COMP2101-S22 is successfully added to /etc/hosts."
+              else 
+              #Failed to add, notify user and exit
+                            echo "The required IP address could not be added in /etc/hosts." >&2
+                            exit 1
+              fi
+fi
+               
+
+
+
+#####Checking to see if I have apache2 already installed
 #Using which command to see if I have it already
-which apache2 >/dev/null
+sudo lxc exec COMP2101-S22 'which apache2' >/dev/null
 if [ $? -ne 0 ]; then
 #I don't have apache2 already installed, so I need to install it now
                sudo apt install apache2
@@ -56,25 +87,25 @@ else
 fi
 
 
-###Retrieving the default web page from container's web service
+
+
+#####Retrieving the default web page from container's web service
 #Running which command to see if I have curl already installed
-which curl >/dev/null
-if [ $? -ne 0 ]; then
-#I don't have curl already installed, so I need to install it now
+sudo lxc exec COMP2101-S22 'which curl' >/dev/null 
+if [ $? -ne 0 ];then
+#I need to install curl first
                 sudo apt install curl
 else
-#I already have curl 
+#I already have curl installed             
 #Running curl http://COMP2101-S22 command to retrieve the web page
-                curl http://COMP2101-S22
-                if [ $? -ne 0 ];then
-                                echo 'Failed to retrieve the default web page'
+                sudo lxc exec COMP2101-S22 'curl http://COMP2101-S22' 
+                if [ $?-eq 0 ];then
+                #Succeeded in retrieving the default web page
+                               echo "Succeeded in retrieving the default web page."
                 else
-                                echo 'Succeeded in retrieving the default web page'
-                fi
-fi
- 
+                #Failed in retriving the web page
+                               echo "Failed in retrieving the default web page." >&2 
+                exit 1 
+                fi  
+fi 
 
- 
-                
- 
-              
